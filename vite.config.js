7242aa163a1a226834c442d670e836332e9fd5ea
Rami1125 <rami.msarwa1@gmail.com>
2025-09-18 13:29:46 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,17 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  // This tells Vite to copy everything from the 'public' folder to the build output
+  publicDir: 'public',
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
         dashboard: resolve(__dirname, 'dashboard.html'),
-        // הוספנו את קובץ לוח הבקרה המתקדם לתהליך הבנייה
-        dashboardPro: resolve(__dirname, 'dashboard-pro.html')
+        dashboardPro: resolve(__dirname, 'dashboard-pro.html'),
+        adm: resolve(__dirname, 'adm.html')
       },
     },
   },
 });
+
