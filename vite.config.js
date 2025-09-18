da7567872ba44d45e09c90b6f08f22d655f77f9e
Rami1125 <rami.msarwa1@gmail.com>
2025-09-18 14:21:32 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,13 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // This tells Vite to copy everything from the 'public' folder to the build output
-  publicDir: 'public',
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
         dashboard: resolve(__dirname, 'dashboard.html'),
+        // Added the new advanced dashboard to the build process
         dashboardPro: resolve(__dirname, 'dashboard-pro.html'),
-        adm: resolve(__dirname, 'adm.html')
       },
     },
   },
